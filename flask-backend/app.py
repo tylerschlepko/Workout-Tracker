a7d8fce63f609f28e1c10fea2b13b61356394cc3
tylerschlepko <97323708+tylerschlepko@users.noreply.github.com>
2023-04-15 20:36:22 -0500

    Made sql file and tested python flask

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,1 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1qaz2wsx@localhost/worktrack'
+
+@app.get('/api')
+def test():
+    test = {'name': 'Tyler', 'job' : 'SWE'}
+    return jsonify(test)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
