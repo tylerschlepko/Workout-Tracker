8f1c0991a2c1aef5344f9cdd08071e8b014210e4
tylerschlepko <97323708+tylerschlepko@users.noreply.github.com>
2023-04-26 14:52:02 -0500

    Added submit button and mostly done with login

--- react/src/components/Login.jsx
+++ react/src/components/Login.jsx
@@ -1,19 +1,18 @@
-import React,{useState} from 'react';
-import Box from '@mui/material/Box';
-import IconButton from '@mui/material/IconButton';
-import Input from '@mui/material/Input';
-import FilledInput from '@mui/material/FilledInput';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormHelperText from '@mui/material/FormHelperText';
-import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Button from "@mui/material-next/Button";
 
 function Login() {
   const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
@@ -21,22 +20,54 @@ function Login() {
     event.preventDefault();
   };
 
+  const handleChange = (e, set) => {
+    set(e.target.value);
+  };
+
+  const handleClick = (e) => {
+    console.log(`Email: ${email} \nPassword: ${password}`);
+  };
+
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems:'center', height:'100vh' }}>
-    <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-        <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-        <InputLabel htmlFor='email-input'>Email</InputLabel>
-        <OutlinedInput
-        id='email-input'
-        type='email'
-        label="email"
-        />
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <FormControl sx={{ m: 1, width: "30ch" }} variant="outlined">
+          <InputLabel htmlFor="email-input">Email</InputLabel>
+          <OutlinedInput
+            id="email-input"
+            type="email"
+            label="email"
+            value={email}
+            onChange={(e) => {
+              handleChange(e, setEmail);
+            }}
+          />
         </FormControl>
-      <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+        <FormControl sx={{ m: 1, width: "30ch" }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
           <OutlinedInput
             id="outlined-adornment-password"
-            type={showPassword ? 'text' : 'password'}
+            value={password}
+            onChange={(e) => {
+              handleChange(e, setPassword);
+            }}
+            type={showPassword ? "text" : "password"}
             endAdornment={
               <InputAdornment position="end">
                 <IconButton
@@ -52,7 +83,15 @@ function Login() {
             label="Password"
           />
         </FormControl>
-    </Box>
+        <Box onClick={handleClick} sx={{ marginTop: "10px", borderRadius: '25px'}}>
+        <Button
+          variant="filledTonal"
+          sx={{ height: "50px", width:'100%' }}
+        >
+          Login
+        </Button>
+        </Box>
+      </Box>
     </Box>
   );
 }
